d747c078603d7dd026c5985d46537438ba17212d
Vahid <vahid>
2025-05-25 00:05:19 +0530

    Expense tracker CRUD operations fixed

--- Services/ExpensesService.cs
+++ Services/ExpensesService.cs
@@ -1,85 +1,77 @@
 ﻿using AzureTestApp.Models;
 using System.Collections.Generic;
 
-
 namespace AzureTestApp.Services
 {
     public class ExpensesService
     {
         HttpClient client;
-        //public Expenses formData = new Expenses();
         private AppDbContext _databaseContext;
-        public ExpensesService(AppDbContext databaseContext, HttpClient client)
+        IConfiguration _configuration;
+        private readonly string? apiURL;
+
+        public ExpensesService(AppDbContext databaseContext, HttpClient client, IConfiguration configuration)
         {
             _databaseContext = databaseContext;
             this.client = client;
+            _configuration = configuration;
+            apiURL = _configuration["ApiUrl"];
         }
         public async Task<List<Expenses>> GetAllTransactionsByMonth(int month, int year)
         {
-            List < Expenses > expenses = new List<Expenses> ();
-            string url = $"http://localhost:3344/api/Purse/GetTxByMonth?month={month}&year={year}";
+            List<Expenses> expenses = new List<Expenses>();
+            string url = $"{apiURL}/api/Purse/GetTxByMonth?month={month}&year={year}";
             var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 expenses = await response.Content.ReadFromJsonAsync<List<Expenses>>();
             }
             return expenses;
-
-            //return _databaseContext.Tbl_Expenses.Where(tx => tx.DayOfTransaction.Month == month && tx.DayOfTransaction.Year == year).ToList();
         }
-
         public async Task<double> GetTotalByMonth(int month, int year)
         {
             Double total = 0;
-            string url = $"http://localhost:3344/api/Purse/GetTotalByMonth?month={month}&year={year}";
+            string url = $"{apiURL}/api/Purse/GetTotalByMonth?month={month}&year={year}";
             var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 total = await response.Content.ReadFromJsonAsync<Double>();
             }
             return total;
-            //return _databaseContext.Tbl_Expenses.Where(tx => tx.DayOfTransaction.Month == month && tx.DayOfTransaction.Year == year).Sum(x => x.Amount);
         }
-
         public async Task<List<CatageoryAmount>> GetByCatageory(int month, int year)
         {
             List<CatageoryAmount> catAmount = new List<CatageoryAmount>();
-            string url = $"http://localhost:3344/api/Purse/GetCatageoryWiseByMonth?month={month}&year={year}";
+            string url = $"{apiURL}/api/Purse/GetCatageoryWiseByMonth?month={month}&year={year}";
             var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 catAmount = await response.Content.ReadFromJsonAsync<List<CatageoryAmount>>();
             }
             return catAmount;
-            //return _databaseContext.Tbl_Expenses
-            //    .Where(tx => tx.DayOfTransaction.Month == month && tx.DayOfTransaction.Year == year)
-            //    .GroupBy(x=> x.Catageory)
-            //    .Select(g => new CatageoryAmount { Catageory = g.Key, TotalAmount = g.Sum(x=> x.Amount)}).ToList();
         }
-
-
-
         public async Task AddTransaction(Expenses expenses)
         {
-            string url = "http://localhost:3344/api/Purse/AddTx";
+            string url = $"{apiURL}/api/Purse/AddTx";
             var response = await client.PostAsJsonAsync(url, expenses);
-            if (response.IsSuccessStatusCode)
-            {
-
-            }
-            //_databaseContext.Tbl_Expenses.Add(expenses);
-            //_databaseContext.SaveChanges();
+            if (response.IsSuccessStatusCode){}
         }
-
         public async Task<Expenses> GetTransactionbyId(int Id)
         {
-           var result = _databaseContext.Tbl_Expenses.Where(x=> x.Id == Id).FirstOrDefault();
+            var result = _databaseContext.Tbl_Expenses.Where(x => x.Id == Id).FirstOrDefault();
             return result;
         }
-
+        public async Task UpdateTransaction(int id, Expenses expenses)
+        {
+            string url = $"{apiURL}/api/Purse/UpdateTx?id={id}";
+            var response = await client.PutAsJsonAsync(url, expenses);
+            if (response.IsSuccessStatusCode) { }
+        }
         public async Task DeleteTransaction(int Id)
-        { 
-        
+        {
+            string url = $"{apiURL}/api/Purse/DeleteTx?txId={Id}";
+            var response = await client.PutAsync(url, null);
+            if (response.IsSuccessStatusCode) { }
         }
         public List<int> GetYears()
         {
@@ -91,45 +83,43 @@ namespace AzureTestApp.Services
             }
             return years;
         }
-
         public Dictionary<int, string> GetMonths()
         {
             Dictionary<int, string> monthDict = new Dictionary<int, string>();
-            for (int m = 1; m <= 12; m++)
-            {
-                if (m == 1)
-                    monthDict.Add(m, "January");
-                if (m == 2)
-                    monthDict.Add(m, "February");
-                if (m == 3)
-                    monthDict.Add(m, "March");
-                if (m == 4)
-                    monthDict.Add(m, "April");
-                if (m == 5)
-                    monthDict.Add(m, "May");
-                if (m == 6)
-                    monthDict.Add(m, "June");
-                if (m == 7)
-                    monthDict.Add(m, "July");
-                if (m == 8)
-                    monthDict.Add(m, "August");
-                if (m == 9)
-                    monthDict.Add(m, "September");
-                if (m == 10)
-                    monthDict.Add(m, "October");
-                if (m == 11)
-                    monthDict.Add(m, "November");
-                if (m == 12)
-                    monthDict.Add(m, "December");
-            }
+            monthDict.Add(1, "January");
+            monthDict.Add(2, "February");
+            monthDict.Add(3, "March");
+            monthDict.Add(4, "April");
+            monthDict.Add(5, "May");
+            monthDict.Add(6, "June");
+            monthDict.Add(7, "July");
+            monthDict.Add(8, "August");
+            monthDict.Add(9, "September");
+            monthDict.Add(10, "October");
+            monthDict.Add(11, "November");
+            monthDict.Add(12, "December");
             return monthDict;
         }
-
-        public void UpdateTransaction() 
+        public Dictionary<string, string> GetCatageories()
         {
-        
+            Dictionary<string, string> catDict = new Dictionary<string, string>();
+            catDict.Add("EMI", "EMI");
+            catDict.Add("House", "House");
+            catDict.Add("Transport", "Transport");
+            catDict.Add("SuperMarket", "SuperMarket");
+            catDict.Add("Online", "Online");
+            catDict.Add("Subscriptions", "Subscriptions");
+            catDict.Add("CreditCard", "CreditCard");
+            catDict.Add("Rice", "Rice");
+            catDict.Add("Others", "Others");
+            return catDict;
         }
-
-
+        public Dictionary<string, string> GetTransactionType()
+        {
+            Dictionary<string, string> TxTypeDict = new Dictionary<string, string>();
+            TxTypeDict.Add("Debited", "Debited");
+            TxTypeDict.Add("Credited", "Credited");
+            return TxTypeDict;
+        }
     }
 }
